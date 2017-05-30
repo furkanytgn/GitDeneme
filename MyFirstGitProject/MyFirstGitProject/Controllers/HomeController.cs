a111584575f3981dcf763a0277c68eb6a5d106c3
Furkan YETGIN <furkan.yetgin@bilgeadam.com>
2017-05-31 00:36:26 +0300

    first commit

---
+++ MyFirstGitProject/MyFirstGitProject/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstGitProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
